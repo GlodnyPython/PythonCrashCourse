83126abe338ae5981cb0ec3353be7dd0ab365763
matijot <41494546+matijot@users.noreply.github.com>
2018-07-21 03:37:15 +0200

    Zabawa ze zmiennymi

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,12 @@
-print("Hello python world");
-print("");
-print("python jest glodny");
-print("puthon chce bobra");
+print("Hello python world")
+print("")
+print("python jest glodny")
+print("puthon chce bobra")
+
+print("========================================")
+
+message = "Helo Python World"
+print(message)
+
+message = "Hello Python Crash Course world"
+print(message)
