90ad6b55caec6de27283f923ecaec832625ca478
matijot <41494546+matijot@users.noreply.github.com>
2018-07-21 03:38:39 +0200

    Drobne korekty

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,11 +1,11 @@
 print("Hello python world")
 print("")
 print("python jest glodny")
-print("puthon chce bobra")
+print("python chce bobra")
 
 print("========================================")
 
-message = "Helo Python World"
+message = "Hello Python World"
 print(message)
 
 message = "Hello Python Crash Course world"
