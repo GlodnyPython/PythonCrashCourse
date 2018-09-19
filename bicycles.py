30d93268e092756bd9dfc539323a16f600c7ff70
matijot <41494546+matijot@users.noreply.github.com>
2018-09-19 21:43:55 +0200

    duży push

--- bicycles.py
+++ bicycles.py
@@ -2,3 +2,8 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 print(bicycles[0])
 print(bicycles[0].title())
+print(bicycles[-1]) # -1 zawsze oznacza ostatni item w liscie
+print(bicycles[-2]) # -2 i kolejne ocznaczaja odpowiednio 2 i kolejne itemy od konca
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
